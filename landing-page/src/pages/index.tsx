4b0d3fc4712ce506a7f898068f0f161a735e7dc3
Felipe <felipee.trova@gmail.com>
2021-03-23 21:10:25 -0300

    Update SectionAgenda to get data from API

--- landing-page/src/pages/index.tsx
+++ landing-page/src/pages/index.tsx
@@ -18,7 +18,16 @@ import client from 'graphql/client'
 import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
 import { LandingPageProps } from 'types/api'
 
-const Index = ({ logo, header, sectionAboutProject, sectionTech, sectionConcepts, sectionModules }: LandingPageProps) => (
+const Index = (
+  {
+    logo,
+    header,
+    sectionAboutProject,
+    sectionTech,
+    sectionConcepts,
+    sectionModules,
+    sectionAgenda
+  }: LandingPageProps) => (
   <>
     <SectionHero
       logo={logo}
@@ -28,7 +37,7 @@ const Index = ({ logo, header, sectionAboutProject, sectionTech, sectionConcepts
     <SectionTech {...sectionTech} />
     <SectionConcepts {...sectionConcepts} />
     <SectionModules {...sectionModules} />
-    <SectionAgenda />
+    <SectionAgenda {...sectionAgenda} />
     <PricingBox />
     <SectionAboutUs />
     <SectionReviews />
